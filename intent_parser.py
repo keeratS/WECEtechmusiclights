83e0816ffb9f2ad252501355116e9b85ea2881ea
megnabb2 <megnabb2@illinois.edu>
2021-01-09 05:42:35 -0800

    added to the intent parser

--- intent_parser.py
+++ intent_parser.py
@@ -5,7 +5,10 @@ import time
 
 # local imports
 from weather_lights.wind_speed_lights import wind_speed_lights
-
+from weather_lights.short_forecast import short_forecast
+from lights_test.galaxy_effect import galaxy_lights
+from lights_test.rainbow_shift import rainbow_shift
+from lights_test.neopixel_rpi_simpletest import simple_lights_test
 # helper to run a given function as a subprocess
 # for functions that run forever, (i.e. only stop on ctrl-C)
 # allows us to stop based on some time
@@ -37,8 +40,24 @@ def parse_intent(command, pixels, w):
     
     # TODO actual implementation -- need to correctly map command to function
     # currently only example of how code would work
-    if("weather" in command):
+    if("wind speed" in command): #works
         run_func_sub(wind_speed_lights, (w, pixels), 5, pixels)
-
+    #not reformatted yet
+    #if("temperature" in command):
+        #run_func_sub(today_temp, (w, pixels), 5, pixels)
+    if("weather" in command):
+        run_func_sub(short_forecast, (w, pixels), 5, pixels)
+        
+    if("fireworks" in command):
+        run_func_sub(firework_lights, (pixels), 5, pixels)
+    if("galaxy" in command):
+        run_func_sub(galaxy_lights, pixels, 5, pixels)
+    if("lights" in command):
+        run_func_sub(simple_lights_test, pixels, 5, pixels)
+    if("rainbow" in command): #works
+        run_func_sub(rainbow_shift, (w, pixels), 5, pixels)
     
+    
+    
+        
     
