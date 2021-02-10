bd8987c75777b4e857640590cafdc8a5e2e5ab62
sah128 <lgf7391pL@outlook.com>
2021-02-09 21:19:33 -0600

    transition added

--- weather_lights/twentyfour_hours.py
+++ weather_lights/twentyfour_hours.py
@@ -21,62 +21,67 @@ def get_hourly_temps(w, pixels):
             
             while True:
             #need if statements first, then go through each for loop
-                if i in range (0, 21): #white, and then goes to halfway blue
+                if weather in range (0, 21): #white, and then goes to halfway blue
                     red=255
                     sub1 = 21-weather #for loop to then add the darkness of blue, each value back 12.75
                     if not (sub1 == 0):
                         for a in range (0, sub1):
                                 red=red-12
-                
-                pixels.fill((red, 255, 255))
-                pixels.show()
-                time.sleep(10)
+                    for a in range (50):
+                        pixels[a]= (red, 255, 255)
+                        pixels.show()
+                        time.sleep(10)
              
                     
-                if i in range (21, 41): #blue to halfway green
-                    blue=255
+                if weather in range (21, 41): #blue to halfway green
                     sub2= 41-weather
+                    blue=255
                     if not (sub2 == 0):
                         for i in range (0, sub2):
-                            blue=blue-12 
-                pixels.fill((0, 255, blue))
-                pixels.show()
-                time.sleep(10)
+                            blue=blue-12
+                    
+                    
+                    for b in range (50):
+                        pixels[b]=(0, 255, blue)
+                        pixels.show()
+                        time.sleep(10)
                      
              
-                if i in range (41, 61):# green to halfway yellow
+                if weather in range (41, 61):# green to halfway yellow
                     sub3 = 61-weather
                     red2=255
                     if not (sub3 == 0):
                         for b in range (0, sub3):
                              red2= red2-12 #changing red gives us yellow with b=0
-                
-                pixels.fill((red2, 255, 0))
-                pixels.show()
-                time.sleep(10)
+                    for c in range (50):
+                        pixels[c]=(red2, 255, 0)
+                        pixels.show()
+                        time.sleep(10)
             
-                if i in range (61, 81):#yellow, halfway red (orange should be the result)
+                if weather in range (61, 81):#yellow, halfway red (orange should be the result)
                     sub4= 81- weather
                     green=255
                     if not (sub3 ==0):
                      for c in range (0, sub4):
                     #halfway to red in order to get orange
                          green= green-6
-                    
-                    pixels.fill((255, green, 0))
-                    pixels.show()
-                    time.sleep(10)
+                    for d in range (50):
+                        pixels[d]=(255, green, 0)
+                        pixels.show()
+                        time.sleep(10)
             
-            if i in range (81, 101): #orange to red
-                red2=255
-                green2= 127
-                sub5=101-weather
-                if not (sub5 == 0):
-                 for d in range (0, sub5):
-                    green2=green2-12 #the lowest B value will be 0, giving us total green
-                 pixels.fill((red2, green2, 0))
-                 pixels.show()
-                 time.sleep(10)
+                if weather in range (81, 101): #orange to red
+                    red2=255
+                    green2= 127
+                    sub5=101-weather
+                    if not (sub5 == 0):
+                         for d in range (0, sub5):
+                            green2=green2-12 #the lowest B value will be 0, giving us total green
+                    
+                    for e in range (50):
+                        pixels[e]=(red2, green2, 0)
+                        pixels.show()   
+                        time.sleep(10)
 
                
                 #could implement some sort of effect here.
