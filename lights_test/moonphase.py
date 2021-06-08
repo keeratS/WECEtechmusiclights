b5bf58bf6860686d0357a1ee7fefddab12f937e7
Keerat Singh <keerat2012@gmail.com>
2021-06-07 19:29:20 -0700

    testing moon phase code, not integrated into commands yet

---
+++ lights_test/moonphase.py
@@ -0,0 +1,56 @@
+#moon illumination reporting code
+
+import time
+import board
+import neopixel
+
+import random
+
+def gradient(pixels,shift):
+  '''This function creates a gradient'''
+  #colors to gradient in RGB
+  bcolor=(167,158,235) #bright color in RGB
+  ncolor=(216,222,255) #neutral color in RGB
+
+  #calculations
+  colorstep = tuple(map(lambda i, j: i - j, bcolor, ncolor)) #subtract tuples from each other
+  colorstep = colorstep/pixels.n
+
+  for i in range(pixels):
+      i=(i+shift)%pixels.n
+    pixels[i]=(bcolor[0]+colorstep[0]*i,bcolor[1]+colorstep[1]*i,bcolor[2]+colorstep[2]*i)
+
+
+from bs4 import BeautifulSoup
+from urllib import request
+def moonshine(pixels):
+  '''  This function communicates how much of the moon is illuminated at the time by referencing a specific website. '''
+
+  url = "https://www.moongiant.com/phase/today/" #this website tells moon info
+
+  #extraction of relevant information using beautifulsoup to parse html of website
+  html = request.urlopen(url).read()
+  soup = BeautifulSoup(html, 'html.parser')
+  small_soup = soup.select("#moonDetails")[0] #go to the moonDetails id
+  croutons = small_soup.find_all('span') #get a list of all the span items
+
+  illumination = croutons[1].get_text() #illumination is second in the list of span items
+  illumination = int(illumination[:-2])/100 #turn it into a number for calculation
+  print ("this much of the moon is illuminated:")
+  print(illumination)
+
+  #find how much of the string lights should be dark
+  darkstart = int(pixels.n*illumination)
+  gradient(pixels.n,0)
+  pixels.show() #turn on the pixels under the gradient to show the command was received
+  time.sleep(4)
+
+  #turn on the lights according to above calculation
+  offset = 0;
+  while ("wece"=="wece")
+    gradient(pixels.n,offset)
+    for i in range(darkstart,pixels.n+1):
+        pixels[1]=(0,0,0) #turn off all pixels except
+
+    pixels.show()
+    offset=(offset+1)%50
